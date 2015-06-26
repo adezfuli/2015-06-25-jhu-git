e81b0b87cff7a587b6f77937128a3d4e741cc4d5
Amin Dezfuli <amindezfuli@yahoo.com>
2015-06-26 10:35:26 -0400

    finished up rest of temp. conversion

--- src/temperature.py
+++ src/temperature.py
@@ -1,2 +1,12 @@
 def f_to_k(temp):
 	return ((temp - 32) * (5./9.)) + 273.15
+
+def k_to_c(temp):
+        return temp - 273.15
+
+def k_to_c(temp):
+	temp_k = f_to_k(temp)
+	temp = k_to_v(temp)
+	return temp
+
+
